4ae875b59c5283f0efd9475d9946137ee3c5eb82
evilmeryl <evilmeryl@gmail.com>
2023-04-10 22:06:42 +0200

    models.py Added more methods

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from clcrypto import hash_password
 
 class User:
     pass
-    def __init__(self, username, password, salt=''):  # we can add second param:"salt"
+    def __init__(self, username, password='', salt=''):  # we can add second param:"salt"
         self._id = -1
         self.username = username
         self._hashed_password = hash_password(password, salt)  # we can add second param:"salt"
@@ -25,7 +25,7 @@ class User:
 
     def save_to_db(self, cursor):
         if self._id == -1:
-            sql = f"""INSERT INTO users(username, hashed_password)
+            sql = """INSERT INTO users(username, hashed_password)
                     VALUES (%s, %s) RETURNING id;
                     """
             cursor.execute(sql, (self.username, self._hashed_password))
@@ -33,15 +33,48 @@ class User:
             return True
         return False
 
+    @staticmethod
+    def load_user_by_username(cursor, username):
+        sql = "SELECT id, username, hashed_password FROM users WHERE username=%s"
+        cursor.execute(sql, (username, ))
+        data = cursor.fetchone()
+        if data:
+            id_, username, hash_password = data
+            load_user = User(username)
+            load_user._id = id_
+            load_user._hashed_password = hash_password
+            return load_user
+        return None
+
+    @staticmethod
+    def load_user_by_id(cursor, id_):
+        sql = "SELECT id, username, hashed_password FROM users WHERE id=%s"
+        cursor.execute(sql, (id_, ))
+        data = cursor.fetchone()
+        if data:
+            id_, username, hashed_password = data
+            load_user = User(username)
+            load_user._id = id_
+            load_user._hashed_password = hashed_password
+            return load_user
+        return None
+
 u1 = User("marcin", "TestNr1")
-u1.hashed_password = "nowehaslo"
-print(u1.hashed_password)
+u3 = User("marcin", "testnr2")
+# u1.hashed_password = "nowehaslo"
+# print(u1.hashed_password)
 try:
     cnx = connect(user="postgres", password="coderslab", host="localhost", database="users_db")
     cnx.autocommit = True
-    u1.save_to_db(cnx.cursor())
+    # u1.save_to_db(cnx.cursor())
+    # u3.save_to_db(cnx.cursor())
+    u2 = User.load_user_by_username(cnx.cursor(), "marcin")
+    u4 = User.load_user_by_id(cnx.cursor(), 2)
 except ProgrammingError as PE:
     print(PE)
-
 else:
     cnx.close()
+print(u2.id())
+print(u2.username)
+print(u2.hashed_password)
+print(u4.id(), u4.username, u4.hashed_password)
